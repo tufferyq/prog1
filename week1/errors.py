fdacc3bd5783ee690b674c4bd4c3ed4419b69a72
Quentin <quentin.tuffery@gmail.com>
2024-08-27 17:36:32 +0300

    update errors.py

--- week1/errors.py
+++ week1/errors.py
@@ -1,8 +1,35 @@
-# COMP.CS.100
-# Creator: Quentin Tuffery
-# Student id number: dvb366
+"""
+The price of a single ride bus ticket in Tampere
+and surrounding areas on Aug 23rd, 2020.
+
+The rules used by the program are:
+
+  -----  -------
+   Age    Price
+  -----  -------
+   >24     2.04
+  17-24    1.47
+   7-16    1.02
+   0-6     0.00
+
+Limited usefulness, the actual rules are more complex.
+"""
 
 def main():
+    string = input("Please, enter your age: ")
+    age = int(string)
 
-    if __name__ == "__main__":
-        main()
+    if age >= 25:
+        ticket_price = 2.04
+    elif age >= 17 and age <= 24:
+        ticket_price = 1.47
+    elif age >= 7 and age <= 16:
+        ticket_price = 1.02
+    elif age >= 0 and age <= 6:
+        ticket_price = 0.00
+
+    print("Your ride will cost:",ticket_price)
+
+
+if __name__ == "__main__":
+    main()
