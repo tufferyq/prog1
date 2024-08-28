6b5e96ba36290c5dda70bcfb77f966992160c6d4
Quentin <quentin.tuffery@gmail.com>
2024-08-28 17:31:27 +0300

    added fixprecision and fixspaces projects

---
+++ week2/fixspaces.py
@@ -0,0 +1,15 @@
+# COMP.CS.100
+# Creator: Quentin Tuffery
+# Student id number: dvb366
+
+"""
+This program gives the multiplication table from 1 to 10 for the entered number.
+"""
+
+def main():
+    PI = 3.14159265358979323846
+    print(f"The approximate value of pi is {PI:1.0f} or, if we want to get specific, {PI:5.4f}")
+
+
+if __name__ == "__main__":
+    main()
