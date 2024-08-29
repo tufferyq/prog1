52227f2755e337d529da3e8594198a0aa7ba706e
Quentin <quentin.tuffery@gmail.com>
2024-08-29 15:35:27 +0300

    added dates_of_the_year.py program

---
+++ week2/dates_of_the_year.py
@@ -0,0 +1,24 @@
+# COMP.CS.100
+# Creator: Quentin Tuffery
+# Student id number: dvb366
+
+"""
+This program gives the multiplication table from 1 to 10 for the entered number.
+"""
+
+def main():
+    for month in range(1, 13):
+        if month in [1, 3, 5, 7, 8, 10, 12]:
+            nbdays = 31
+        elif month in [4, 6, 9, 11]:
+            nbdays = 30
+        elif month == 2:
+            nbdays = 28
+
+        for day in range(1, nbdays+1):
+            print(f"{day}.{month}.", end ="\n")
+            day += 1
+        month +=1
+
+if __name__ == "__main__":
+    main()
