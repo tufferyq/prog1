0dec313655aa579ba65e7297abe6ad97a599683d
Quentin <quentin.tuffery@gmail.com>
2024-09-29 19:25:30 +0300

    added dancing_games exercise

---
+++ week7/7.10_dancing_games.py
@@ -0,0 +1,29 @@
+"""
+COMP.CS.100 Programming 1
+Code template for tanssipelit assigment.
+"""
+
+SONG_RESULT = {"Bubble dancer": 93.4, "The Game": 92.03, "Vertex": 75.3,
+               "Lemmings on the Run": 86.2, "Da Roots": 96.02,
+               "Charlene": 75.3, "Disconnected": 86.3, "Fly away": 87.32,
+               "Hybrid": 63.9, "My favourite game": 89.45, "Oasis": 59.5,
+               "Remember December": 96.3, "The beginning": 90.45,
+               "Tribal Style": 87.45, "Why Me": 97.38, "Xuxa": 63.84,
+               "Zodiac": 83.43, "Queen of Light": 75.12, "Mouth": 98.34,
+               "Pandemonium": 79.31}
+
+def calculate_average(dictionary):
+    """
+    this function calculates the average of the values of a dictionary
+    :param dictionary: dict, the values are float
+    :return average: float, average of all the values of the dict
+    """
+    values = list(dictionary.values())
+    average = sum(values) / len(values)
+    return average
+
+def main():
+    print(calculate_average(SONG_RESULT))
+
+if __name__ == "__main__":
+    main()
